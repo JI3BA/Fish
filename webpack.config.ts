6d21c7bfd315ac4415895dfb900e44576eca3c44
JI3BA <lyovkin_artyom@mail.ru>
2023-03-19 13:58:47 +0300

    added file,html,css loader in my project for normal interaction with redux store

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,14 +1,26 @@
+const path = require('path');
+
 module.exports = {
+  entry: './src/index.ts',
   module: {
     rules: [
       {
-        test: /\.(png|jpe?g|gif|jpg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'src/images',
       },
     ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'src/images/[name][ext]'
   },
 };
