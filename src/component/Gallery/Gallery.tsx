27c56fc7f49c7633436e3648ef44a48b846b9f98
JI3BA <lyovkin_artyom@mail.ru>
2023-03-25 19:17:52 +0300

    install react transition group for animation slider

--- src/component/Gallery/Gallery.tsx
+++ src/component/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 import '../../styles/Gallery.css';
 import '../../styles/Button.css';
 import { useAppSelector } from "../../hooks/redux";
@@ -9,14 +9,22 @@ import Button from "../Button/Button";
 
 const Gallery: FC = () => {
     const fishImage = useAppSelector(state => state.FishReducer.fish)
-   
+
+    const [activeIndex, setActiveIndex] = useState<number>(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+        setActiveIndex(activeIndex === fishImage.length - 1 ? 0 : activeIndex + 1);
+        }, 10000);
+        return () => clearInterval(interval);
+    }, [activeIndex]);
 
     return(
         <div className="gallery">
             <div className="gallery__container wrapper">
                 {fishImage.map((item,index) => {
                     return(
-                        <div className="image__container" key={index}>
+                        <div className={index === activeIndex ? "image__container active" : "inactive"} key={index}>
                             <img src={item.picture} className="gallery__image" alt={item.name} />
                             <div className="gallery__description">
                                 <h2 className="image__title">{item.name}</h2>
@@ -26,8 +34,8 @@ const Gallery: FC = () => {
                     )
                 })}
                 
-                <Button className="button__slider button__prev">{'<'}</Button>
-                <Button className="button__slider button__next">{'>'}</Button>
+                <Button className="button__slider button__prev" onClick={() => setActiveIndex(activeIndex < 1 ? fishImage.length - 1 : activeIndex - 1)}>{'<'}</Button>
+                <Button className="button__slider button__next" onClick={() =>  setActiveIndex(activeIndex === fishImage.length - 1 ? 0 : activeIndex + 1)}>{'>'}</Button>
             </div>
         </div>
     )
