30c03dda94ba2f9c6f2efc1b0eb991178cb719f1
JI3BA <lyovkin_artyom@mail.ru>
2023-03-30 01:35:29 +0300

    working on Modal component

--- src/component/Modal/ModalRecipes/ModalRecipes.tsx
+++ src/component/Modal/ModalRecipes/ModalRecipes.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
 import { isModal } from "../../../store/reducers/ModalSlice";
 import Button from "../../Button/Button";
+import '../../../styles/ModalRecipes.css';
+
 
 const ModalRecipes = () => {
     const recipeFish = useAppSelector(state => state.ModalRecipes.recipe)
@@ -10,27 +12,28 @@ const ModalRecipes = () => {
     const { closeModal } = isModal.actions
 
     return(
-        <div className="modal">
-            {recipeFish && 
-                <div className="modalll">
-                    <div className="modal__container">
+        <>
+            {modal && <div className="modal" onClick={() => dispatch(closeModal())}>
+                {recipeFish && 
+                    <div className="modal__container" onClick={(e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation()}>
                         <img src={recipeFish[0].picture} className='modal__picture recipes__picture' alt={recipeFish[0].name}/>
                         <div className="modal__description recipes__description">
-                            <h3 className='modal__description recipes__name'>{recipeFish[0].name}</h3>
-                            <p className="recipes__line"></p>
-                            <div className="modal__description recipes__ingredients">
+                            <h3 className='modal__name recipes__name'>{recipeFish[0].name}</h3>
+                            <div className="modal__ingredients recipes__ingredients">
                                 {recipeFish[0].ingredients.map((item,index) => {
                                     return(
-                                        <p className="modal__description ingredients__item" key={index}>{item};</p>
+                                        <p className="modal__item ingredients__item" key={index}>{item};</p>
                                         )
                                     })}
                             </div>
                             <Button className='modal__close' onClick={() => dispatch(closeModal())}>Close</Button>
+                            <p className="modal_close--cross" onClick={() => dispatch(closeModal())}>✖</p>
                         </div>
                     </div>
-                </div>
+                }
+            </div>
             }
-        </div>
+        </>
     )
 }
 
--- src/component/Recipes/Recipes.tsx
+++ src/component/Recipes/Recipes.tsx
@@ -19,32 +19,34 @@ const Recipes: FC = () => {
     }
 
     return(
-        <div className="recipes">
-            <div className="recipes__container wrapper">
-                <h1 className="title__component recipes__title">Recipes</h1>
-                <div className='card__container'>
-                    {recipesFist.map((item,index) => {
-                        return(
-                            <div className='recipes__card' key={index} onClick={() => addModalRecipe(index)}>
-                                <img src={item.picture} className='recipes__picture' alt={item.name}/>
-                                <div className="recipes__description">
-                                    <h3 className='recipes__name'>{item.name}</h3>
-                                    <p className="recipes__line"></p>
-                                    <div className="recipes__ingredients">
-                                        {item.ingredients.map((item,index) => {
-                                            return(
-                                                <p className="ingredients__item" key={index}>{item};</p>
-                                            )
-                                        })}
+        <>
+            <div className="recipes">
+                <div className="recipes__container wrapper">
+                    <h1 className="title__component recipes__title">Recipes</h1>
+                    <div className='card__container'>
+                        {recipesFist.map((item,index) => {
+                            return(
+                                <div className='recipes__card' key={index} onClick={() => addModalRecipe(index)}>
+                                    <img src={item.picture} className='recipes__picture' alt={item.name}/>
+                                    <div className="recipes__description">
+                                        <h3 className='recipes__name'>{item.name}</h3>
+                                        <p className="recipes__line"></p>
+                                        <div className="recipes__ingredients">
+                                            {item.ingredients.map((item,index) => {
+                                                return(
+                                                    <p className="ingredients__item" key={index}>{item};</p>
+                                                )
+                                            })}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            )
-                        })}
-                    </div>
+                                )
+                            })}
+                        </div>
+                </div>
             </div>
             <ModalRecipes />
-        </div>
+        </>
     )
 }
 
