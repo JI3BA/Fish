b4935abd2e50e396fc29ce1777e3df51941c1463
JI3BA <lyovkin_artyom@mail.ru>
2023-07-01 17:56:52 +0300

    fixed CartSlice reducers -> addFish,ChangeAmount,disabledAmount

--- src/store/reducers/CartSlice.ts
+++ src/store/reducers/CartSlice.ts
@@ -13,16 +13,16 @@ export const CartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        addFish: (state, action: PayloadAction<IFish>): void => {
-            state.cart?.push(action.payload)
+        addFish: (state,action:PayloadAction<IFish>): void => {
+            state['cart']?.push(action.payload)
         },
         changeAmount: (state, action: PayloadAction<Omit<IFish, 'id' | 'description' | 'weight'
         | 'picture' | 'price' | 'isCart' | 'minAmount' | 'disabled'>>): void => {
-            state.cart?.map(item => item.name === action.payload.name ? item.amount = action.payload.amount : item)
+            state['cart']?.map(item => item.name === action.payload.name ? item.amount = action.payload.amount : item)
         },
         disabledAmount: (state, action: PayloadAction<Omit<IFish, 'id' | 'description' | 'weight'
         | 'picture' | 'price' | 'minAmount' | 'isCart' >>): void => {
-            state.cart?.map(item => item.name !== action.payload.name ? item : (item.minAmount === action.payload.amount) ? item.disabled = true : item.disabled = false)
+            state['cart']?.map(item => item.name !== action.payload.name ? item : (item.minAmount === action.payload.amount) ? item.disabled = true : item.disabled = false)
         }
     }
 })
