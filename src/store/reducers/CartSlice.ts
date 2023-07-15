b34db5a364c2d9e632d3cba4a85b1740b8ec60d3
JI3BA <lyovkin_artyom@mail.ru>
2023-07-15 21:43:25 +0300

    added reducer for remove order

--- src/store/reducers/CartSlice.ts
+++ src/store/reducers/CartSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import { IFish } from "../../models/IFish";
 
 interface CartState {
@@ -14,21 +14,24 @@ export const CartSlice = createSlice({
     initialState,
     reducers: {
         addFish: (state,action:PayloadAction<IFish>): void => {
-            state['cart']?.push(action.payload)
+            state['cart'].push(action.payload)
+        },
+        removeFish: (state, action: PayloadAction<IFish>): void => {
+            state['cart'] = state['cart'].filter(item => action.payload.name !== item.name)
         },
         changeAmount: (state, action: PayloadAction<Omit<IFish, 'id' | 'description' | 'weight'
         | 'picture' | 'price' | 'isCart' | 'minAmount' | 'disabled'>>): void => {
-            state['cart']?.map(item => item.name === action.payload.name
+            state['cart'].map(item => item.name === action.payload.name
                 ? item.amount = action.payload.amount
                 : item)
         },
         disabledAmount: (state, action: PayloadAction<Omit<IFish, 'id' | 'description' | 'weight'
         | 'picture' | 'price' | 'minAmount' | 'isCart' >>): void => {
-            state['cart']?.map(item => item.name !== action.payload.name
+            state['cart'].map(item => item.name !== action.payload.name
                 ? item
                 : (item.minAmount === action.payload.amount)
-                    ? item.disabled = true
-                    : item.disabled = false)
+                    ? item.disabled
+                    : !item.disabled)
         }
     }
 })
