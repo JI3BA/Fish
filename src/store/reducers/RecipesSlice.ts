8331a90a52853b9799eb47f0a4807a4c7ff15fc1
JI3BA <lyovkin_artyom@mail.ru>
2023-09-02 20:55:09 +0300

    adding steps to recipes

--- src/store/reducers/RecipesSlice.ts
+++ src/store/reducers/RecipesSlice.ts
@@ -20,14 +20,19 @@ const initialState: RecipesState = {
         ingredients: ['Salmon Steak: 300g', 'Olive oil (or sesame): 2tablespoon', 'Lemons: 0.5pieces', 'Salt (or soy sauce): taste',
         'Ground red pepper (not hot): 0.5teaspoons', 'Italian herbs: 0.5teaspoons', 'Ground black pepper: taste', 'Thyme, thyme (fresh or rosemary): taste'],
         picture: salmonSteak,
-        time: '40 minutes'
+        time: '40 minutes',
+        step1: 'Season salmon on both sides with salt and pepper. In a large skillet over medium-high heat, heat oil and butter. Add salmon and cook until bottom is golden, 5 minutes, then flip. Add garlic, lemon juice, honey, and red pepper flakes. Cook until salmon is cooked through, another 5 minutes, spooning sauce over salmon as it cooks.',
+        step2: 'Garnish with parsley before serving.'
     },
     {
         id: 1,
         name: 'Tuna Steak',
         ingredients: ['Tuna Steak: 400g', 'Lemons: 1pieces', 'Salt: taste', 'Peppercorns: taste', 'Vegetable oil: 20g', 'Greenery: taste'],
         picture: tunaSteak,
-        time: '30 minutes'
+        time: '30 minutes',
+        step1: 'Mix orange juice, soy sauce, olive oil, parsley, lemon juice, garlic, oregano, and pepper together in a large non-reactive dish until well combined. Place tuna steaks in marinade and turn to coat. Cover the dish with plastic wrap and marinate in the refrigerator for at least 30 minutes.',
+        step2: 'Preheat an outdoor grill for high heat and lightly oil the grate. Remove tuna steaks from the marinade and shake off excess; reserve marinade for basting.',
+        step3: 'Cook tuna steaks on the preheated grill for 5 to 6 minutes; flip steaks and baste with reserved marinade. Cook for an additional 5 minutes, or to desired doneness. Discard any remaining marinade.'
     },
     {
         id: 2,
