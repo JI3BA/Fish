1d9921d1523a42645a83e3a5e5c309f259c6863e
JI3BA <lyovkin_artyom@mail.ru>
2023-09-03 18:37:05 +0300

    created Recipe component

---
+++ src/pages/Recipe/Recipe.tsx
@@ -0,0 +1,40 @@
+
+
+export const Recipe = () => {
+    return(
+        <div className="recipe">
+            <div className="recipe__container wrapper">
+                <div className="preview">
+                    <img src="" alt="" className="preview__image"/>
+                    <h3 className='modal__name recipes__name'></h3>
+                </div>
+                <div className="steps">
+                    <div className="step">
+                        <p className="step__text"></p>
+                        <span className="step__line"></span>
+                    </div>
+
+                    <div className="step">
+                        <p className="step__text"></p>
+                        <span className="step__line"></span>
+                    </div>
+
+                    <div className="step">
+                        <p className="step__text"></p>
+                        <span className="step__line"></span>
+                    </div>
+
+                    <div className="step">
+                        <p className="step__text"></p>
+                        <span className="step__line"></span>
+                    </div>
+
+                    <div className="step">
+                        <p className="step__text"></p>
+                        <span className="step__line"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
