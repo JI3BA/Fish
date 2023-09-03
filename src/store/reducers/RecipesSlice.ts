1c96733226b29b0a28fc6bfa99ad5080610203d3
JI3BA <lyovkin_artyom@mail.ru>
2023-09-03 18:35:56 +0300

    added steps to RecipesSlice

--- src/store/reducers/RecipesSlice.ts
+++ src/store/reducers/RecipesSlice.ts
@@ -39,28 +39,45 @@ const initialState: RecipesState = {
         name: 'Cutlets from Pollock',
         ingredients: ['Pollock fillet: 500g', 'Potato: 1pieces', 'Eggs: 1pieces', 'Salt: taste', 'Herb Blend (Dried): taste', 'Sunflower oil: 1tablespoon'],
         picture: cutletsPollock,
-        time: '35 minutes'
+        time: '35 minutes',
+        step1: 'In a Cup put the sour cream. Add enough warm boiled water, pour soy sauce. Thoroughly with fork to whisk the mixture.',
+        step2: 'To the mixture add the breadcrumbs, stir and leave to swell.\n' +
+            'Meanwhile, Alaska Pollock fillets, cut into small pieces, and onion. Garlic clean.\n' +
+            'Skip the onion garlic and the fillets through a meat grinder or use a blender.',
+        step3: 'Fish forcemeat add the egg and soaked breadcrumbs, add the salt and pepper mixture.\n' +
+            'Add the semolina and butter are at room temperature.\n' +
+            'Knead carefully minced by hand and put in the fridge for half an hour.\n' +
+            'Then form patties and fry both sides in vegetable oil.'
     },
     {
         id: 3,
         name: 'Salted Mackerel',
         ingredients: ['Mackerel: 2pieces', 'Water: 0.5liters', 'Salt: 2tablespoon', 'Sugar: 1teaspoons', 'Peppercorns: 10pieces', 'Bay leaf: 3pieces'],
         picture: saltedMackerel,
-        time: '8 hours'
+        time: '8 hours',
+        step1: 'Salt is used not only to season the fish, but it also extracts water from the fish and eliminates any unpleasant smell. Once you sprinkle the mackerel with salt, let it rest for 20 minutes. Do not let it sit anytime shorter or longer. If it is rested too short a time, the fish does not soak up enough salty flavor and the smell may still linger. On the other hand, if you rest it for too long, the smell may be gone but the flesh will get too tight and lose its texture.'
     },
     {
         id: 4,
         name: 'Salted Trout',
         ingredients: ['Trout: 350g', 'Sea salt: 2tablespoons', 'Sugar: 1teaspoons', 'Dill: 1teaspoons', 'Bay leaf: 1pieces'],
         picture: saltedTrout,
-        time: '2 days'
+        time: '2 days',
+        step1: 'Trout, cleaned of scales and entrails.',
+        step2: 'Remove the head, tail (do not throw away these parts, prepare the ear), fins, cut the fish in half - lengthwise, remove the bones. Prepare the fillets without skin. Pat them dry with a paper towel. Add salt to each half on both sides. Put most of the salt on the thickened edges.',
+        step3: 'Put the fish in a plastic bag or a suitable-sized fish dish (dishware). I can stand the fish for a day and eat it, if it seems to you that this time is not enough, increase the salting time to the desired. The fish turns out crispy, delicious, not burdened with other flavors.'
     },
     {
         id: 5,
         name: 'Halibut in the oven in foil',
         ingredients: ['Halibut: 500g', 'Sunflower oil: 10g', 'Ground black pepper: taste', 'Salte: taste', 'Bulb onions: 30g', 'Tomatoes: 40g', 'Lemon juice: taste'],
         picture: halibutOven,
-        time: '30 minutes'
+        time: '30 minutes',
+        step1: 'Preheat outdoor grill to medium heat or indoor oven to 400 degrees. Prepare 4 sheets of heavy-duty aluminum foil at least 16" x 12". Season each halibut filet on all sides with salt and pepper then set aside.',
+        step2: 'Spray the center of each piece of foil with cooking spray and add a quarter of the sliced zucchini to each sheet. Divide the onion and bell pepper evenly among each packet and season all the veggies lightly with salt and pepper.',
+        step3: 'Place one seasoned filet on top of each bed of veggies and sprinkle with your choice of chopped fresh herb and parsley. Lay 2–3 slices of lemon on top of each piece of fish and drizzle approximately 1 tsp of olive oil over the fish and veggies.',
+        step4: 'Fold long sides of foil up and over and bring edges together. Roll the foil together, moving downward until 1–2" from the top of the fish. Fold both short ends together to seal the packet but make sure to leave enough space inside the packet for steam expansion.',
+        step5: 'Place packets directly on grill or on a baking sheet in the oven. Grill them 8–9 minutes or bake 12–14 minutes in the oven or until veggies are tender and fish is barely cooked through. Remove from grill or oven and carefully open packets to allow steam to escape. Enjoy!'
     },
 ]}
 
