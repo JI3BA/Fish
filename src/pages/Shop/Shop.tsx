5f92c585db5f598baacea600c7c8a8b65cfbcbaf
JI3BA <lyovkin_artyom@mail.ru>
2023-09-06 15:48:10 +0300

    added component Footer to Shop page

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,11 +1,13 @@
 import { Header } from '../../component/Header/Header'
 import { Main } from '../../component/Main/Main'
+import {Footer} from "../../component/Footer/Footer";
 
 export const Shop = () => {
     return(
         <>
             <Header />
             <Main />
+            <Footer />
         </>
     )
 }
